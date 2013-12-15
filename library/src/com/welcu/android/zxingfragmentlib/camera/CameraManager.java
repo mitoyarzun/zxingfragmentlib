2d4ca976eba617199f9aa9c00fac3f40a0de4259
eBuildy <thomasdecaux@ebuildy.com>
2013-12-15 16:27:13 +0100

    Merged ebuildy's patch.

--- library/src/com/welcu/android/zxingfragmentlib/camera/CameraManager.java
+++ library/src/com/welcu/android/zxingfragmentlib/camera/CameraManager.java
@@ -43,8 +43,8 @@ public final class CameraManager {
 
   private static final int MIN_FRAME_WIDTH = 240;
   private static final int MIN_FRAME_HEIGHT = 240;
-  private static final int MAX_FRAME_WIDTH = 540;
-  private static final int MAX_FRAME_HEIGHT = 540;
+  private static final int MAX_FRAME_WIDTH = 2000;
+  private static final int MAX_FRAME_HEIGHT = 2000;
 
   private final Context context;
   private final View view;
