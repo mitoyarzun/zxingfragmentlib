28b1be2e0dc704d028c8e90839367e023a1a541b
Jaime Oyarzun <mito.oyarzun@gmail.com>
2014-01-20 18:20:56 -0300

    Updated sample to include torch usage #7.

--- sample/src/com/welcu/android/zxingfragmentlibsample/SampleActivity.java
+++ sample/src/com/welcu/android/zxingfragmentlibsample/SampleActivity.java
@@ -2,14 +2,40 @@ package com.welcu.android.zxingfragmentlibsample;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import com.welcu.android.zxingfragmentlibsample.R;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.widget.Button;
+
+import com.welcu.android.zxingfragmentlib.BarCodeScannerFragment;
 
 /**
  * Created by mito on 9/17/13.
  */
 public class SampleActivity extends FragmentActivity {
+
+    boolean torchState = false;
+
+    Button mToggleButton;
+    BarCodeScannerFragment mScannerFragment;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.welcu.android.zxingfragmentlibsample.R.layout.sample_activity);
+
+        FragmentManager fm = getSupportFragmentManager();
+        mScannerFragment = (BarCodeScannerFragment) fm.findFragmentById(R.id.scanner_fragment);
+
+        mToggleButton = (Button) findViewById(R.id.button_flash);
+        mToggleButton.setOnClickListener(createToggleFlashListener());
+    }
+
+    private View.OnClickListener createToggleFlashListener() {
+        return new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            torchState = !torchState;
+            mScannerFragment.setTorch(torchState);
+          }
+        };
     }
 }
